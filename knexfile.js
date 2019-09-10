bf07108548339581722c644deeb549f8b82fb469
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-10 02:17:36 +0100

    WIP: generated users database

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './users.sqlite3'
-    }
+      filename: './users.db3'
+    },
+    useNullAsDefault: true
   },
-  useNullAsDefault: true
+
 };
